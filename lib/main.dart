bd83c3d5bd527807e53074bc8d750e71ddee559d
Alexander Dmitriev <alexander.dmitriev2000@yandex.ru>
2022-04-09 22:06:06 +0300

    The app name has been changed

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:qr/qr.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QrApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class QrApp extends StatelessWidget {
+  const QrApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
