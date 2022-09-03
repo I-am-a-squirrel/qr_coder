521026063ec8a10a8daeda5441fdd68c774762d1
I-am-a-squirrel <86472122+I-am-a-squirrel@users.noreply.github.com>
2022-09-04 00:48:24 +0300

    Archtectural error Moving database logic to State

--- lib/widgets/bloc/color_scheme_cubit.dart
+++ lib/widgets/bloc/color_scheme_cubit.dart
@@ -1,13 +1,5 @@
-import 'dart:io';
-
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:objectdb/objectdb.dart';
-import 'package:objectdb/src/objectdb_storage_filesystem.dart';
-import 'package:objectdb/src/objectdb_storage_indexeddb.dart';
-
 import 'package:qr_coder/classes/my_custom_theme.dart';
-import 'package:qr_coder/widgets/objectdb/color_theme_objectdb_schema.dart';
 
 //Cubit to change global Color scheme
 
@@ -15,70 +7,15 @@ class ColorSchemeCubit extends Cubit<MyCustomTheme> {
 
 	ColorSchemeCubit(MyCustomTheme initialTheme) : super(initialTheme); 
 
-	var regexAll = RegExp(RegExp.escape(''), caseSensitive: false);
-
-	factory ColorSchemeCubit.fromDB() {
-		final Platform userDevice = Platform();
-	
-		if (userDevice.isAndroid || userDevice.isLinux) {
-			final path = Directory.current.path + '/theme.db';
-			var storage = FileSystemStorage(path);
-		}
-
-		if (userDevice.isWeb) {
-			var storage = IndexedDBStorage('theme');
-		}
-		
-		final db = SchemaDB<ColorThemeObjectdbSchema> (
-			storage,
-			(themeMap) => ColorThemeObjectdbSchema.fromMap(themeMap),
-		);
-
-		if (isDbEmpty(db)) {
-			return ColorSchemeCubit(
-				MyCustomTheme(
-					red: true,
-					green: true,
-					blue: true,
-				)
-			);
-		}else{
-			return ColorSchemeCubit(
-				themeFromDB(db)
-			);
-		}
-	}
-
-	static bool isDbEmpty(SchemaDB<ColorThemeObjectdbSchema> db) {
-		var result = db.find({
-			'themeObject': regexAll
-		});
-		result.then((ColorThemeObjectdbSchema resultSchema) {
-			return resultSchema.toMap().isEmpty;
-		});
-	}
-
-	static MyCustomTheme themeFromDB(SchemaDB<ColorThemeObjectdbSchema> db) {
-		var result = db.first({
-			'themeObject': regexAll
-		});
-		result.then((ColorThemeObjectdbSchema theme) {
-			return theme.toMap()['themeObject'];
-		});
-	}
-
 	void toggleRed() {
-		var newState = state.changedRed;
-		emit(newState);
+		emit(state.changedRed);
 	}
 
 	void toggleGreen() {
-		var newState = state.changedGreen;
-		emit(newState);
+		emit(state.changedGreen);
 	}
 
 	void toggleBlue() {
-		var newState = state.changedBlue;
-		emit(newState);
+		emit(state.changedBlue);
 	}
 }
--- lib/widgets/states/my_home_page_state.dart
+++ lib/widgets/states/my_home_page_state.dart
@@ -14,11 +14,63 @@ import 'package:qr_coder/widgets/stateful/my_body.dart';
 import 'package:qr_coder/widgets/stateful/my_home_page.dart';
 
 class MyHomePageState extends State<MyHomePage> {
-	final AdvancedDrawerController advancedDrawerController = AdvancedDrawerController();
+
+	MyCustomTheme? defaultTheme;
+	
+	final regexAll = RegExp(RegExp.escape(''), caseSensitive: false);
+
+	bool isDbEmpty(SchemaDB<ColorThemeObjectdbSchema> db) {
+		var result = db.find({
+			'themeObject': regexAll
+		});
+		result.then((ColorThemeObjectdbSchema resultSchema) {
+			return resultSchema.toMap().isEmpty;
+		});
+	}
+
+	MyCustomTheme themeFromDB(SchemaDB<ColorThemeObjectdbSchema> db) {
+		var result = db.first({
+			'themeObject': regexAll
+		});
+		result.then((ColorThemeObjectdbSchema theme) {
+			return theme.toMap()['themeObject'];
+		});
+	}
+
+	void initTheme(db) {
+		if(isDbEmpty(db)) {
+			defaultTheme = MyCustomTheme(
+				red: true,
+				green: true,
+				blue: true,
+			);
+		}else{
+			defaultTheme = themeFromDB(db);
+		}
+	}
+
+	@override
+	void initState() {
+		super.initState();
+		final AdvancedDrawerController advancedDrawerController = AdvancedDrawerController();
+		final Platform userDevice = Platform();
+		final path = (userDevice.isAndroid || userDevice.isLinux) ? (Directory.current.path + '/theme.db') : '';
+		final storage = path == '' ? IndexedDBStorage('theme') : FyleSystemStorage(path);
+		final db = SchemaDB<ColorThemeObjectdbSchema> (
+			storage,
+			(themeMap) => ColorThemeObjectdbSchema.fromMap(themeMap),
+		);
+		initTheme(db);
+	}
+
+	@override
+	void dispose() {
+		super.dispose();
+	}
 
   Widget build(BuildContext context) {
 		return BlocProvider(
-			create: (context) => ColorSchemeCubit(),
+			create: (context) => ColorSchemeCubit(defaultTheme),
 			child: BlocBuilder<ColorSchemeCubit, MyCustomTheme>(
 				builder: (context, state) {
 					return Theme(
